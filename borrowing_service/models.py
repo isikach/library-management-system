ea22bf2c978cb75e47ec63e106b7e3aa1968128f
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-01 15:24:08 +0300

    refactor(borrowing_service): remove on_delete param from book field

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -6,18 +6,17 @@ from book_service.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
+    borrow_date = models.DateField(auto_now_add=True,)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, blank=True)
+    actual_return_date = models.DateField(null=True, blank=True,)
     book = models.ManyToManyField(
         Book,
-        on_delete=models.CASCADE,
-        related_name="borrowings"
+        related_name="borrowings",
     )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="borrowings"
+        related_name="borrowings",
     )
 
     class Meta:
