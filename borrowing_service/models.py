8da237188699de52bc1890a515236c864a6004d0
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-01 17:38:37 +0300

    feat(borrowing_service): add BorrowingViewSet with list & retrieve actions

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -9,9 +9,10 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True,)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True,)
-    book = models.OneToOneField(
-        Book, on_delete=models.PROTECT,
-        related_name="borrowing"
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.PROTECT,
+        related_name="borrowings"
     )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
