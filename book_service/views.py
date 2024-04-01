348bfb748f9f04a3a6e78bdaff646b367e1d78c2
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-04-01 17:09:52 +0300

    add book_service/views.py, book_service/urls.py, add blank line in book_service/serializers.py and url to book_service to library_management_system/urls.py

--- book_service/views.py
+++ book_service/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from rest_framework.filters import SearchFilter, OrderingFilter
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+
+from book_service.models import Book
+from book_service.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["title", "author"]
+    ordering_fields = ["author"]
+
+    def get_permissions(self):
+
+        if self.action in ["update", "create", "partial_update", "delete"]:
+            return [permissions.IsAdminUser()]
+
+        elif self.action == "list":
+            return [permissions.IsAuthenticatedOrReadOnly()]
+
+        elif self.action == "retrieve":
+            return [permissions.IsAuthenticated()]
+
+        return super().get_permissions()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="search",
+                description=(
+                        "Search books by title or author,"
+                        " ?search=title/author"
+                ),
+                type=str,
+                required=False,
+            ),
+            OpenApiParameter(
+                name="ordering",
+                description="Order books by author ?ordering=author",
+                type=str,
+                required=False,
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """
+        Displays the list f all books.
+        """
+        return super().list(request, *args, **kwargs)
