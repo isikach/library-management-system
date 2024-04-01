d5041c7883c846da338db8791f0e2b63b3349751
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-04-01 16:21:31 +0300

    feat(book_service): left one serializer

--- book_service/serializers.py
+++ book_service/serializers.py
@@ -1,21 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from book_service.models import Book
 
 
-class BookSerializer(ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = "__all__"
-
-
-class BookListSerializer(ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ["title", "author", "inventory"]
-
-
-class BookDetailSerializer(ModelSerializer):
-    class Meta:
-        model = Book
-        fields = "__all__"
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee",)
