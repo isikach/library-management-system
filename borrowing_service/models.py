d2a6f7741b8d754e845be3fe69fe57a5823eab91
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-01 16:03:54 +0300

    refactor(borrowing_service): refactor models.py according to PEP8

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -9,7 +9,10 @@ class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True,)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True,)
-    book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name="borrowing")
+    book = models.OneToOneField(
+        Book, on_delete=models.CASCADE,
+        related_name="borrowing"
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
