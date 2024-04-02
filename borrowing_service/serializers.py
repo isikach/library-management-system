19d488434000f0ec1246607853d902155517a7ba
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-02 07:48:29 +0300

    feat(borrowing_service): implement borrowing creation

--- borrowing_service/serializers.py
+++ borrowing_service/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 
 from borrowing_service.models import Borrowing
@@ -12,25 +13,41 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
             "book",
-            "user",
         )
         read_only_fields = ("actual_return_date", "borrow_date",)
+
+    def validate(self, attrs):
+        data = super(BorrowingSerializer, self).validate(attrs)
+        Borrowing.validate_book_inventory(
+            attrs["book"].inventory,
+            attrs["book"].title
+        )
+        return data
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            book = validated_data.pop("book")
+            book.inventory -= 1
+            book.save()
+
+            return Borrowing.objects.create(**validated_data)
 
 
 class BorrowingDetailSerializer(BorrowingSerializer):
     # TODO book=BookDetailSerializer
     user = serializers.SlugRelatedField(
         read_only=True,
-        slug_field="email"
+        slug_field="email",
     )
 
 
 class BorrowingListSerializer(BorrowingSerializer):
     user = serializers.SlugRelatedField(
         read_only=True,
-        slug_field="email"
+        slug_field="email",
     )
     book = serializers.SlugRelatedField(
-        slug_field="title"
+        slug_field="title",
+        read_only=True,
     )
 
