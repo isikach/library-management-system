b85a4cb2f627247649612245ab042b6a23debf9a
Artem Hryshko <artemggg2222@gmail.com>
2024-04-02 15:35:54 +0300

    feat(user): redefine create for CreateUserView & add permission

--- user/views.py
+++ user/views.py
@@ -1,11 +1,24 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data = {
+            "data": serializer.data,
+            "you need to log in to telegram": "https://t.me/book_borrowing_bot",
+        }
+        return Response(data=data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
