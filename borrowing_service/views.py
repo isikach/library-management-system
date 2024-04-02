c504b9b22939305b7f55dc35832a6a7783f4bee0
Vlad Kyianytsia <kiyanitsa2002021@gmail.com>
2024-04-02 07:48:29 +0300

    feat(borrowing_service): implement borrowing creation

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -7,10 +7,14 @@ from borrowing_service.serializers import BorrowingSerializer, BorrowingDetailSe
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -20,3 +24,6 @@ class BorrowingViewSet(
             return BorrowingDetailSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
