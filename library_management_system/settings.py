e59570ed9b563390b28e975a4ff764b0b171e395
Artem Hryshko <artemggg2222@gmail.com>
2024-04-03 14:20:49 +0300

    refactor(user): change permission class

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES":
-        ("user.permissions.IsAdminOrIfAuthenticatedOrIsActiveReadOnly",),
+        ("user.permissions.IsAuthenticatedAndIsActive",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5
 }
