aba3068353ed474c4c703bee96787d8a09238e10
Artem Hryshko <artemggg2222@gmail.com>
2024-04-03 14:20:49 +0300

    refactor(user): change permission class

--- user/permissions.py
+++ user/permissions.py
@@ -1,11 +1,10 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class IsAdminOrIfAuthenticatedOrIsActiveReadOnly(BasePermission):
+class IsAuthenticatedAndIsActive(BasePermission):
 
     def has_permission(self, request, view):
         return (
-            request.method in SAFE_METHODS
-            and request.user.is_authenticated
+            request.user.is_authenticated
             and request.user.is_active
-        ) or request.user.is_staff
+        )
