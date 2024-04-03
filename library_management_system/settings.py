3b0bf1d229aa4ff44c51cc369a141cf62d876d99
Artem Hryshko <artemggg2222@gmail.com>
2024-04-03 12:56:36 +0300

    feat(user): add DEFAULT_PAGINATION_CLASS

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -143,6 +143,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES":
         ("user.permissions.IsAdminOrIfAuthenticatedOrIsActiveReadOnly",),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5
 }
 
 SPECTACULAR_SETTINGS = {
