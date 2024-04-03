f9d7076da7776734e03f5315e688087cbe72753e
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-04-03 13:10:33 +0300

    feat(book_service): fixed schema in view.py & validators in model -> to 0.00 & 1000.00

--- book_service/models.py
+++ book_service/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
     daily_fee = models.DecimalField(
         max_digits=5,
         decimal_places=2,
-        validators=[MinValueValidator(0), MaxValueValidator(1000)]
+        validators=[MinValueValidator(0.00), MaxValueValidator(1000.00)]
     )
 
     def __str__(self):
