75f3c362eb0f0427ad6059ea861b3a1e85ea8244
illiasikach <sikatch.ilja@gmail.com>
2024-04-03 10:01:42 +0300

    feat (notification service): implemented schedule notifications

---
+++ notification_service/users_with_overdue.py
@@ -0,0 +1,53 @@
+from typing import List
+
+from django.contrib.auth import get_user_model
+from django_q.tasks import schedule
+from django.utils import timezone
+
+from notification_service.telegram_bot import BookBorrowingBot
+from user.models import User
+from borrowing_service.models import Borrowing
+
+
+def get_overdue_users() -> List[User]:
+    today = timezone.now().date()
+    overdue_borrowers = Borrowing.objects.filter(
+        actual_return_date__isnull=True,
+        expected_return_date__lt=today
+    )
+    overdue_users = overdue_borrowers.values_list("user", flat=True).distinct()
+    users_with_overdue_borrowings = get_user_model().objects.filter(id__in=overdue_users)
+
+    return users_with_overdue_borrowings
+
+
+def get_overdue_books_for_user(user_id: int) -> str:
+    today = timezone.now().date()
+    overdue_borrowings = Borrowing.objects.filter(
+        user_id=user_id,
+        actual_return_date__isnull=True,
+        expected_return_date__lt=today
+    )
+    overdue_books_titles = [borrowing.book.title for borrowing in overdue_borrowings]
+    return "\n".join(overdue_books_titles)
+
+
+def notification_about_overdue() -> None:
+    overdue_users = get_overdue_users()
+    for user in overdue_users:
+        books = get_overdue_books_for_user(user.user_id)
+        BookBorrowingBot().overdue_borrowing_notification(
+            user=user, books=books)
+
+
+schedule_time = timezone.localtime(timezone.now()).replace(
+    hour=9,
+    minute=0,
+    second=0
+)
+
+schedule(
+    "notification_about_overdue",
+    schedule_type="D",
+    next_run=schedule_time
+)
